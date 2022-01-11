aab4e7581dfff7e9a614368dab576a0c06d3448f
bazithiago <thiagovieira.dev@gmail.com>
2022-01-11 19:50:07 -0300

    First configs

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HomepageStyles = styled.div`
+    color: red;
+`
+
+export default function Homepage() {
+    return(
+        <HomepageStyles>
+            Olá mundo!
+        </HomepageStyles>
+    )
+}
